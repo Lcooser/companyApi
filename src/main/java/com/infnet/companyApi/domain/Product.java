a88a2bf6b3340d6eda425b284fff8c3cc7e3f6d1
Luiz Coser <luiz.cooser1@gmail.com>
2024-06-02 16:23:53 -0300

    First commit, initial files structure

---
+++ src/main/java/com/infnet/companyApi/domain/Product.java
@@ -0,0 +1,4 @@
+package com.infnet.companyApi.domain;
+
+public class Product {
+}
---
+++ src/test/java/com/infnet/companyApi/CompanyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.companyApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
